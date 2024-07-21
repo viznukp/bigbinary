5b9918e08f246c2d476c2872fb9e2cb0f6e09463
viznukp <vishnukp.me@gmail.com>
2024-07-21 22:42:37 +0530

    Added ability to set quantity of cart items

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -41,7 +41,15 @@ const Product = () => {
 
   if (isError) return <PageNotFound />;
 
-  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
+  const {
+    name,
+    description,
+    mrp,
+    offerPrice,
+    imageUrls,
+    imageUrl,
+    availableQuantity,
+  } = product;
 
   const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
@@ -68,7 +76,7 @@ const Product = () => {
           <Typography className="font-semibold text-green-600">
             {discountPercentage}% off
           </Typography>
-          <AddToCart {...{ slug }} />
+          <AddToCart {...{ availableQuantity, slug }} />
         </div>
       </div>
     </>
