b0ca27b2247ec4048706cb9dc8cacb9c674d2ef9
viznukp <vishnukp.me@gmail.com>
2024-07-23 04:22:45 +0530

    Created React query custom hook to implement parallel queries

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,10 +1,8 @@
-import { useState, useEffect } from "react";
-
-import { NoData, Toastr } from "@bigbinary/neetoui";
-import productsApi from "apis/products";
+import { NoData } from "@bigbinary/neetoui";
 import { PageLoader } from "components/commons";
 import Header from "components/commons/Header";
 import { cartTotalOf } from "components/utils";
+import { useFetchCartProducts } from "hooks/reactQuery/useProductsApi";
 import i18n from "i18next";
 import { keys, isEmpty } from "ramda";
 import useCartItemsStore from "stores/useCartItemsStore";
@@ -16,46 +14,12 @@ import ProductCard from "./ProductCard";
 import { MRP, OFFER_PRICE } from "../constants";
 
 const Cart = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
+  const slugs = useCartItemsStore(store => keys(store.cartItems));
 
-  const slugs = keys(cartItems);
+  const { data: products = [], isLoading } = useFetchCartProducts(slugs);
 
   const totalMrp = cartTotalOf(products, MRP);
   const totalOfferPrice = cartTotalOf(products, OFFER_PRICE);
-
-  const fetchCartProducts = async () => {
-    try {
-      const responses = await Promise.all(
-        slugs.map(slug => productsApi.show(slug))
-      );
-
-      setProducts(responses);
-
-      responses.forEach(({ availableQuantity, name, slug }) => {
-        if (availableQuantity >= cartItems[slug]) return;
-
-        setSelectedQuantity(slug, availableQuantity);
-        if (availableQuantity === 0) {
-          Toastr.error(
-            `${name} is no longer available and has been removed from cart`,
-            {
-              autoClose: 2000,
-            }
-          );
-        }
-      });
-    } catch (error) {
-      console.log("An error occurred:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCartProducts();
-  }, [cartItems]);
 
   if (isLoading) return <PageLoader />;
 
